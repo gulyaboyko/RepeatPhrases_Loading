e3594cf09ec49befc6d3ba09bde313e404cc8727
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-01-09 14:31:34 +0300

    Initial Commit

---
+++ RepeatPhrases_LoadingTests/RepeatPhrases_LoadingTests.swift
@@ -0,0 +1,34 @@
+//
+//  RepeatPhrases_LoadingTests.swift
+//  RepeatPhrases_LoadingTests
+//
+//  Created by Gulya Boiko on 1/9/20.
+//  Copyright © 2020 com.appres. All rights reserved.
+//
+
+import XCTest
+@testable import RepeatPhrases_Loading
+
+class RepeatPhrases_LoadingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
