3c931cec03a5fa8c5e2de07dcdbf606632ddb6ff
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-01-21 17:27:41 +0300

    Add FirestoreHTTPClient

---
+++ RepeatPhrases_Loading/API/AudioModel/FirestoreHTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  FirestoreHTTPClient.swift
+//  RepeatPhrases_Loading
+//
+//  Created by Gulya Boiko on 1/21/20.
+//  Copyright © 2020 com.appres. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct FirestoreHTTPClient: HTTPClient {
+    
+    func get(url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
+        
+    }
+}
